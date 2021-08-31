9f5d7107d21bef8e4463a31464022aee67741f22
GermanQ <ing.germanquintero@gmail.com>
2021-08-30 21:10:35 -0500

    Agregando Taller-4: Analisis de Salarios

---
+++ Taller-4/salarios.js
@@ -0,0 +1,121 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Carlos",
+    salary: 1200,
+});
+colombia.push({
+    name: "Juan",
+    salary: 3000,
+});
+colombia.push({
+    name: "German",
+    salary: 1500,
+});
+colombia.push({
+    name: "Adrian",
+    salary: 1000,
+});
+colombia.push({
+    name: "Pepe",
+    salary: 2500,
+});
+colombia.push({
+    name: "Dayanna",
+    salary: 1800,
+});
+colombia.push({
+    name: "Camila",
+    salary: 800,
+});
+colombia.push({
+    name: "Ruben",
+    salary: 4000,
+});
+colombia.push({
+    name: "Dolly",
+    salary: 3200,
+});
+colombia.push({
+    name: "Diana",
+    salary: 1700,
+});
+colombia.push({
+    name: "Geiner",
+    salary: 1300,
+});
+colombia.push({
+    name: "Jorge",
+    salary: 1000,
+});
+colombia.push({
+    name: "Yina",
+    salary: 2000,
+});
+colombia.push({
+    name: "Armando",
+    salary: 2500,
+});
+colombia.push({
+    name: "William",
+    salary: 1800,
+});
+colombia.push({
+    name: "Julio",
+    salary: 2300,
+});
+colombia.push({
+    name: "Yesid",
+    salary: 1600,
+});
+colombia.push({
+    name: "Yeiso",
+    salary: 1000,
+});
+colombia.push({
+    name: "Salome",
+    salary: 1700,
+});
+colombia.push({
+    name: "Josue",
+    salary: 3000,
+});
+colombia.push({
+    name: "Alisson",
+    salary: 1900,
+});
+colombia.push({
+    name: "Jose",
+    salary: 3400,
+});
+colombia.push({
+    name: "Julio",
+    salary: 2300,
+});
+colombia.push({
+    name: "Yesid",
+    salary: 1600,
+});
+colombia.push({
+    name: "Yeiso",
+    salary: 1000,
+});
+colombia.push({
+    name: "Salome",
+    salary: 6000,
+});
+colombia.push({
+    name: "Josue",
+    salary: 3000,
+});
+colombia.push({
+    name: "Alisson",
+    salary: 5000,
+});
+colombia.push({
+    name: "Jose",
+    salary: 3400,
+});
